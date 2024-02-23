009b1f75c442560e8293d9ba73c0cc19b62f11e7
Ajikuru <Ajikuru@gmail.com>
2024-02-23 08:59:23 +0100

    Update App.tsx

--- chat-app-test/src/App.tsx
+++ chat-app-test/src/App.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3001');
+//const socket = io('http://localhost:3001');
+const socket = io('https://chat-test-backend.vercel.app');
 
 function App() {
     const [messages, setMessages] = useState<Array<string>>([]);
